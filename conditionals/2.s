b9e69298bc718cbdaff4d9588afa0f9b26a3fe4c
KEPCU <kpachac@ulasalle.edu.pe>
2022-09-14 21:12:17 -0500

    Second

--- conditionals/2.s
+++ conditionals/2.s
@@ -1,43 +1,33 @@
 .data
-text1: .asciiz "\n Enter Password: "
-text2: .asciiz "\n Password match"
-text3: .asciiz "\n Password doesn't match"
-password: .asciiz "123"
-str1: .space 20
-str2: .space 20
+    text1: .asciiz "\n Enter Password: "
+    text2: .asciiz "\n Password match"
+    text3: .asciiz "\n Password doesn't match"
+    password: .asciiz "123"
+    buffer: .space 20
 .text
 main:
-
+    # print
     addi $v0,4
     la $a0,text1
     syscall
-
+    # get string 
     li $v0,8
-    la $a0,str1
-    addi $a1,$zero,20
-    syscall   # got string 1
+    la $a0,buffer
+    li $a1,20
+    syscall   
 
-    li $v0,4
-    la $a0,text1
-    syscall
+    la $a0,password  # pass address of buffer
+    la $a1,buffer  # pass address of buffer
+    jal IF2  # call strcmp
 
-    li $v0,8
-    la $a0,str2
-    addi $a1,$zero,20
-    syscall   # got string 2
-
-    la $a0,str1  # pass address of str1
-    la $a1,str2  # pass address of str2
-    jal strcmp  # call strcmp
-
-    beq $v0,$zero,ok # check result
+    beq $v0,$zero,IF # check result
     li $v0,4
     la $a0,text3
     syscall
-    
+
     j Exit
 
-    ok:
+    IF:
         li $v0,4
         la $a0,text2
         syscall
@@ -45,26 +35,26 @@ main:
     li $v0,10
     syscall
 
-    strcmp:
+    IF2:
         add $t0,$zero,$zero
         add $t1,$zero,$a0
         add $t2,$zero,$a1
     Loop:
         lb $t3($t1)  # load a byte from each string
         lb $t4($t2)
-        beqz $t3,checkt2 # str1 end
-        beqz $t4,missmatch
+        beqz $t3,IF3 # str1 end
+        beqz $t4,IF4
         slt $t5,$t3,$t4  # compare two bytes
-        bnez $t5,missmatch
+        bnez $t5,IF4
         addi $t1,$t1,1  # t1 points to the next byte of str1
         addi $t2,$t2,1
         j Loop
 
-    missmatch: 
+    IF4: 
         addi $v0,$zero,1
-        j endfunction
-    checkt2:
-        bnez $t4,missmatch
+        j END_IF
+    IF3:
+        bnez $t4,IF4
         add $v0,$zero,$zero
-    endfunction:
+    END_IF:
     jr $ra
--- loops/13.s
+++ loops/13.s
@@ -1,65 +1,57 @@
 .data
-msg1:.asciiz "Please insert text (max 20 characters): "
-msg2:.asciiz "\nNOT SAME"
-msg3:.asciiz "\nSAME"
-str1: .space 20
-str2: .space 20
+    text1: .asciiz "\n Introduce algo: "
+    text2: .asciiz "\n Text match"
+    text3: .asciiz "\n Text doesn't match"
+    text4: .asciiz "salir"
+    buffer: .space 20
 .text
-.globl main
 main:
-addi $v0,4
-la $a0,msg1
-syscall
-li $v0,8
-la $a0,str1
-addi $a1,$zero,20
-syscall   #got string 1
-li $v0,4
-la $a0,msg1
-syscall
-li $v0,8
-la $a0,str2
-addi $a1,$zero,20
-syscall   #got string 2
+    # print
+    addi $v0,4
+    la $a0,text1
+    syscall
+    # get string 
+    li $v0,8
+    la $a0,buffer
+    li $a1,20
+    syscall   
 
-la $a0,str1  #pass address of str1
-la $a1,str2  #pass address of str2
-jal strcmp  #call strcmp
+    la $a0,text4  # pass address of buffer
+    la $a1,buffer  # pass address of buffer
+    jal IF2  # call strcmp
 
-beq $v0,$zero,ok #check result
-li $v0,4
-la $a0,msg2
-syscall
-j exit
-ok:
-li $v0,4
-la $a0,msg3
-syscall
-exit:
-li $v0,10
-syscall
+    beq $v0,$zero,IF # check result
+    j main
 
-strcmp:
-add $t0,$zero,$zero
-add $t1,$zero,$a0
-add $t2,$zero,$a1
-loop:
-lb $t3($t1)  #load a byte from each string
-lb $t4($t2)
-beqz $t3,checkt2 #str1 end
-beqz $t4,missmatch
-slt $t5,$t3,$t4  #compare two bytes
-bnez $t5,missmatch
-addi $t1,$t1,1  #t1 points to the next byte of str1
-addi $t2,$t2,1
-j loop
+    IF:
+        li $v0,4
+        la $a0,text2
+        syscall
+        j END_IF
+    Exit:
+    li $v0,10
+    syscall
 
-missmatch: 
-addi $v0,$zero,1
-j endfunction
-checkt2:
-bnez $t4,missmatch
-add $v0,$zero,$zero
+    IF2:
+        add $t0,$zero,$zero
+        add $t1,$zero,$a0
+        add $t2,$zero,$a1
+    Loop:
+        lb $t3($t1)  # load a byte from each string
+        lb $t4($t2)
+        beqz $t3,IF3 # str1 end
+        beqz $t4,IF4
+        slt $t5,$t3,$t4  # compare two bytes
+        bnez $t5,IF4
+        addi $t1,$t1,1  # t1 points to the next byte of str1
+        addi $t2,$t2,1
+        j Loop
 
-endfunction:
-jr $ra
+    IF4: 
+        addi $v0,$zero,1
+        j END_IF
+    IF3:
+        bnez $t4,IF4
+        add $v0,$zero,$zero
+    END_IF:
+    jr $ra
